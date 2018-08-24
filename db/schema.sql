722e95939d8d8460dc7cee45455019a6c7101e72
jace <jace.sf84@gmail.com>
2018-08-23 23:52:59 -0700

    changed BLOB to VARCHAR, will not inject images

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE crypto_db;
 CREATE TABLE crypto(
 	id INT NOT NULL AUTO_INCREMENT,
 	currency_name TEXT NOT NULL,
-	currency_symbol BLOB NOT NULL,
+	currency_symbol VARCHAR(255) NOT NULL,
 	currency_price INT NOT NULL, 
 	currency_link VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
