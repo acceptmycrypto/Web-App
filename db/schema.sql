e087ef5652fca1fc6d6c8b30f057d1896b9d5a0c
jace <jace.sf84@gmail.com>
2018-08-23 19:28:31 -0700

    crypto table and columns created

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS crypto_db;
 CREATE DATABASE crypto_db;
 
 USE crypto_db;
+
+CREATE TABLE crypto(
+	id INT NOT NULL AUTO_INCREMENT,
+	currency_name TEXT NOT NULL,
+	currency_symbol BLOB NOT NULL,
+	currency_price INT NOT NULL, 
+	currency_link VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
