0fbcab5a9525d7e7f6869d58a54343fc36b70f5f
jace <jace.sf84@gmail.com>
2018-08-26 20:29:14 -0700

    line 26 to VARCHAR and corrected typo and line 35 changed to crypto_name

--- db/schema.sql
+++ db/schema.sql
@@ -20,19 +20,19 @@ CREATE TABLE crypto_info(
 	FOREIGN KEY (crypto_metadata_id) REFERENCES crypto_metadata(id)
 );
 
--- CREATE TABLE business (
--- 	id INT NOT NULL AUTO_INCREMENT,
--- 	business_name VARCHAR(255) NOT NULL,
--- 	business_discription TEXT NOT NULL,
--- 	business_link VARCHAR(255) NOT NULL,
--- 	PRIMARY KEY (id),
---   FOREIGN KEY business(id) REFERENCES crypto(id)
--- );
+CREATE TABLE business (
+	id INT NOT NULL AUTO_INCREMENT,
+	business_name VARCHAR(255) NOT NULL,
+	business_description VARCHAR NOT NULL,
+	business_link VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id),
+  FOREIGN KEY business(id) REFERENCES crypto(id)
+);
 
--- CREATE TABLE userInput (
--- 	id INT NOT NULL AUTO_INCREMENT,
--- 	user_email VARCHAR(255) NOT NULL,
--- 	cryptoCurrency VARCHAR(255) NOT NULL,
--- 	merchant VARCHAR(255) NOT NULL,
--- 	PRIMARY KEY (id)
--- );
+CREATE TABLE userInput (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_email VARCHAR(255) NOT NULL,
+	crypto_name VARCHAR(255) NOT NULL,
+	merchant VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
