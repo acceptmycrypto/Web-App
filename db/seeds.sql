6b4b6110080c7e0de6023a6fb31f02025ab710d7
Simon Nguyen <simonnguyen3054@gmail.com>
2018-09-01 11:08:17 -0700

    venues.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,71 @@
-INSERT INTO venues
-(venue_name, venue_description, venue_link)
-VALUES
-("Overstock", "an American internet retailer, online store, the company sells home décor, furniture, bedding, and many other goods.", "https://www.overstock.com/");
+-- INSERT INTO venues
+-- (venue_name, venue_description, venue_link)
+-- VALUES
+-- ("Overstock", "an American internet retailer, online store, the company sells home décor, furniture, bedding, and many other goods.", "https://www.overstock.com/");
 
-INSERT INTO venues
-(venue_name, venue_description, venue_link)
-VALUES
-("Microsoft", "an American multinational corp. technology company.", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
+-- INSERT INTO venues
+-- (venue_name, venue_description, venue_link)
+-- VALUES
+-- ("Microsoft", "an American multinational corp. technology company.", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
 
-INSERT INTO cryptos_venues
-(crypto_id, venue_id)
-VALUES
-("1", "1");
+-- INSERT INTO venues
+-- (venue_name, venue_description, venue_link)
+-- VALUES
+-- ("OkCupid:", "internationally operating online dating.", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
 
-INSERT INTO cryptos_venues
-(crypto_id, venue_id)
-VALUES
-("1", "2");
+-- INSERT INTO venues
+-- (venue_name, venue_description, venue_link)
+-- VALUES
+-- ("CheapAir", "an online travel agency", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
 
-INSERT INTO cryptos_venues
-(crypto_id, venue_id)
-VALUES
-("2", "1");
+-- INSERT INTO venues
+-- (venue_name, venue_description, venue_link)
+-- VALUES
+-- ("Etsy", "is an ecommerce focused on handmade or vintage items and supplies.", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
 
-INSERT INTO cryptos_venues
-(crypto_id, venue_id)
-VALUES
-("2", "2");
+-- Bitcoin Cash
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("1", "1");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("1", "2");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("1", "3");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("1", "4");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("1", "5");
+
+-- Bitcoin
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("2", "1");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("2", "2");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("2", "3");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("2", "4");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("2", "5");
+
+-- Dash
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("3", "1");
+
+-- Doge Coin
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("4", "1");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("4", "5");
+
+-- Ethereum Classic
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("5", "1");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("5", "3");
+
+-- Ethereum
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("6", "1");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("6", "3");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("6", "5");
+
+-- Litecoin
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("7", "1");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("7", "2");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("7", "3");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("7", "4");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("7", "5");
+
+-- Monero
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("8", "4");
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("8", "5");
+
+-- XRP
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("9", "5");
+
+-- Verge
+INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("10", "5");
