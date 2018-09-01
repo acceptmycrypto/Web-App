67a4abf292f74f37a8df02be7fa990c70a5d7dda
Simon Nguyen <simonnguyen3054@gmail.com>
2018-09-01 11:21:46 -0700

    add new seed file for cryptos-venues

--- db/seeds_cryptos-venues.sql
+++ db/seeds_cryptos-venues.sql
@@ -1,28 +1,3 @@
--- INSERT INTO venues
--- (venue_name, venue_description, venue_link)
--- VALUES
--- ("Overstock", "an American internet retailer, online store, the company sells home décor, furniture, bedding, and many other goods.", "https://www.overstock.com/");
-
--- INSERT INTO venues
--- (venue_name, venue_description, venue_link)
--- VALUES
--- ("Microsoft", "an American multinational corp. technology company.", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
-
--- INSERT INTO venues
--- (venue_name, venue_description, venue_link)
--- VALUES
--- ("OkCupid:", "internationally operating online dating.", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
-
--- INSERT INTO venues
--- (venue_name, venue_description, venue_link)
--- VALUES
--- ("CheapAir", "an online travel agency", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
-
--- INSERT INTO venues
--- (venue_name, venue_description, venue_link)
--- VALUES
--- ("Etsy", "is an ecommerce focused on handmade or vintage items and supplies.", "https://support.microsoft.com/en-us/help/13942/microsoft-account-add-money-with-bitcoin");
-
 -- Bitcoin Cash
 INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("1", "1");
 INSERT INTO cryptos_venues (crypto_id, venue_id) VALUES ("1", "2");
