dd558fb5577d4109ed67e28fefa942f6d720e4d1
Hooman <36459689+hooman64@users.noreply.github.com>
2018-08-31 18:01:11 -0700

    updated navbar

--- public/assets/javascript/nav1.js
+++ public/assets/javascript/nav1.js
@@ -1,7 +1,7 @@
 // Sticky Header
 $(window).scroll(function() {
 
-    if ($(window).scrollTop() >= 50) {
+    if ($(window).scrollTop() > 100) {
         $('.main_h').addClass('sticky');
     } else {
         $('.main_h').removeClass('sticky');
