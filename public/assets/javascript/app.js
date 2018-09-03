f3a0d6a319b4e369a2c4a30ac1afd3075fe37cc5
jace <jace.sf84@gmail.com>
2018-09-03 13:47:18 -0700

    started query

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -4,9 +4,4 @@ $.ajax({
   }).then(function(data) {
     console.log(data);
     var numOfVenues = 'select count(venue_id) as total from cryptos_venues where crypto_id = 1';
-
-
-
-{/* <div class="cell" data-title="vendors">
-    # of Vendors Accepting (TBD)
-</div> */}
+  });
