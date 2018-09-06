a503053e10d12164121757fb60196638e5665cbb
jace <jace.sf84@gmail.com>
2018-09-05 19:00:10 -0700

    cleaning up code

--- public/assets/javascript/app.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-    url: '/api/cryptos_venues',
-    method: 'GET'
-  }).then(function(data) {
-    console.log(data);
-    var numOfVenues = 'select count(venue_id) as total from cryptos_venues where crypto_id = 1';
-  });
