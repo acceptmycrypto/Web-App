6fbd982bd66f8641c3e6566c5b4a5e7cc46b6c84
ava krishn <avakrishn@gmail.com>
2018-10-07 22:13:46 -0700

    create matched friends components

---
+++ client/src/components/Feed/MatchedFriends/MatchedFriends.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./MatchedFriends.css";
+
+class MatchedFriends extends Component {
+    constructor() {
+      super();
+  
+      this.state = {
+      }
+    }
+  
+  
+    render() {
+      console.log(this.state);
+      return (
+        <div className="MatchedFriends">
+        
+        </div>
+      );
+    }
+  }
+  
+  export default MatchedFriends;
