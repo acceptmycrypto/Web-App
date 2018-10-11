ad76f59c9468f784d81cfd659a0befc8d5185207
jace <jace.sf84@gmail.com>
2018-10-10 21:01:47 -0700

    Jasons changes for signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Signup from './components/Signup';
+import SignUp from './components/SignUp/SignUp';
 import Profile from './components/Profile/UserProfile';
 import Crypto from './components/Crypto';
 import FeedVenues from './components/Feed/Venues';
 import FeedFriends from './components/Feed/MatchedFriends';
 import FeedTransactions from './components/Feed/Transactions';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -14,7 +15,7 @@ class App extends Component {
         <div>
           <ul>
             <li>
-              <Link to="/">Signup</Link>
+              <Link to="/">SignUp</Link>
             </li>
             <li>
               <Link to="/profile">Profile</Link>
@@ -35,12 +36,14 @@ class App extends Component {
 
           <hr />
 
-          <Route exact path="/" component={Signup} />
+          <div>
+          <Route exact path="/" component={SignUp}/>
           <Route path="/profile" component={Profile} />
           <Route path="/crypto" component={Crypto} />
           <Route path="/feed/venues" component={FeedVenues} />
           <Route path="/feed/friends" component={FeedFriends} />
           <Route path="/feed/transactions" component={FeedTransactions} />
+        </div>
         </div>
       </Router>
     );
