8efe242fd460e19926d3c62c7d90e85b4b7df3f6
Simon Nguyen <simonnguyen3054@gmail.com>
2018-10-15 13:38:21 -0700

    add more records for deals table

--- db/seeds_deals.sql
+++ db/seeds_deals.sql
@@ -1,1 +1,7 @@
 INSERT INTO deals (venue_id, deal_name, deal_description, deal_image, pay_in_dollar, pay_in_crypto) VALUES ("6", "Equal Exchange Organic Love Buzz", "1 pound, bulk packaging.", "https://images-na.ssl-images-amazon.com/images/I/51R6iGcdunL.jpg", "13.25", "9.94");
+
+INSERT INTO deals (venue_id, deal_name, deal_description, deal_image, pay_in_dollar, pay_in_crypto) VALUES ("1", '9" H Patch the Pumpkin Stained Glass Accent Lamp', "Option - Patch the Pumpkin Stained Glass Accent Lamp", "https://ak1.ostkcdn.com/images/products/10543610/P17623858.jpg", "82.49", "70.12");
+
+INSERT INTO deals (venue_id, deal_name, deal_description, deal_image, pay_in_dollar, pay_in_crypto) VALUES ("1", 'Safavieh Rug', "Safavieh Casual Natural Fiber Hand-Woven Natural Accents Chunky Thick Jute Rug - 9' x 12'", "https://ak1.ostkcdn.com/images/products/4382772/Safavieh-Casual-Natural-Fiber-Hand-Woven-Natural-Accents-Chunky-Thick-Jute-Rug-9-x-12-ed6e3f9e-04ae-4450-9b44-54fa3f1d60aa_600.jpg", "1296.00", "265.99");
+
+INSERT INTO deals (venue_id, deal_name, deal_description, deal_image, pay_in_dollar, pay_in_crypto) VALUES ("1", 'West Coast Jewelry', "Polished Jasper and Matte Onyx Beaded Stretch Bracelet (10mm Wide)", "https://ak1.ostkcdn.com/images/products/17809279/Polished-Jasper-and-Matte-Onyx-Beaded-Stretch-Bracelet-10mm-Wide-ff63e7d9-f05d-49f3-83b6-c572c5b909c3_600.jpg", "23.04", "16.31");
