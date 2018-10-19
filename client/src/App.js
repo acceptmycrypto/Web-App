756eeffcfd3cccfe8840860ec3f77d025e38456f
Simon Nguyen <simonnguyen3054@gmail.com>
2018-10-19 10:06:53 -0700

    add layout structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,31 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import SignUp from './components/signup/SignUp';
-import SignIn from './components/signup/SignIn';
+import SignUp from "./components/signup/SignUp";
+import SignIn from "./components/signup/SignIn";
 
-import Profile from './components/Profile/UserProfile';
-import Crypto from './components/Crypto';
-import FeedVenues from './components/Feed/Venues';
-import FeedFriends from './components/Feed/MatchedFriends';
-import FeedTransactions from './components/Feed/Transactions';
-import './App.css';
+import Profile from "./components/Profile/UserProfile";
+import Crypto from "./components/Crypto";
+import FeedVenues from "./components/Feed/Venues";
+import FeedFriends from "./components/Feed/MatchedFriends";
+import FeedTransactions from "./components/Feed/Transactions";
+import Layout from "./components/Layout";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/crypto">Crypto</Link>
-            </li>
-            <li>
-              <Link to="/feed/venues">Feed > Venues</Link>
-            </li>
-            <li>
-              <Link to="/feed/friends">Feed > Friends</Link>
-            </li>
-            <li>
-              <Link to="/feed/transactions">Feed > Transactions</Link>
-            </li>
-          </ul>
-
-          <hr />
-
-          <div>
-          <Route exact path="/" component={SignIn}/>
-          <Route path="/SignUp" component={SignUp}/>
-          <Route path="/profile" component={Profile} />
-          <Route path="/crypto" component={Crypto} />
-          <Route path="/feed/venues" component={FeedVenues} />
-          <Route path="/feed/friends" component={FeedFriends} />
-          <Route path="/feed/transactions" component={FeedTransactions} />
-        </div>
+          <Layout />
+            <Route exact path="/" component={SignIn} />
+            <Route path="/SignUp" component={SignUp} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/crypto" component={Crypto} />
+            <Route path="/feed/venues" component={FeedVenues} />
+            <Route path="/feed/friends" component={FeedFriends} />
+            <Route path="/feed/transactions" component={FeedTransactions} />
+          <Layout />
         </div>
       </Router>
     );
