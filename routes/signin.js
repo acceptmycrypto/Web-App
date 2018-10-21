aac1accf3ef63c892fd16d2f1da6387dcc3a5fd1
jace <jace.sf84@gmail.com>
2018-10-20 21:36:52 -0700

    carried over matching SignUp code

---
+++ routes/signin.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+//use sendgrid
+var sgMail = require("@sendgrid/mail");
+var keys = require("../key");
+sgMail.setApiKey(keys.sendgrid);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Your password
+  password: 'password',
+  database: 'crypto_db'
+});
+
+connection.connect(function(err){
+  if(!err) {
+      console.log("Database is connected ... nn");
+  } else {
+      console.log("Error connecting database ... nn");
+  }
+  });
+
+
+
+
+
+
+
+
+app.post('/login', function(req, res) {
+    db.users.findOne({
+        username: req.body.username
+    }, function(error, result) {
+        if (!result) return res.status(404).json({ error: 'user not found' });
+
+        if (!bcrypt.compareSync(req.body.password, result.password)) return res.status(401).json({ error: 'incorrect password ' });
+
+        var payload = {
+            _id: result._id,
+            username: result.username
+        };
+
+        var token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '4h' });
+
+        return res.json({
+            message: 'successfuly authenticated',
+            token: token
+        });
+    });
+})
