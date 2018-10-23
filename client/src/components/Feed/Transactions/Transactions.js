fd002a81b3a902d4b2a30e4e35322b12ec1bd5c7
Simon Nguyen <simonnguyen3054@gmail.com>
2018-10-22 23:59:26 -0700

    add background color to body, add cryptoranking table

--- client/src/components/Feed/Transactions/Transactions.js
+++ client/src/components/Feed/Transactions/Transactions.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Transactions.css";
+import CryptoRankings from "../../CryptosRanking";
 
 class Transactions extends Component {
   constructor() {
@@ -10,9 +11,12 @@ class Transactions extends Component {
     };
   }
 
-  async componentDidMount() {   //es6 Promise syntax
+  async componentDidMount() {
+    //es6 Promise syntax
     //fetch transactions info
-    const data = await fetch("http://localhost:3001/api/transactions/community/payment_received");
+    const data = await fetch(
+      "http://localhost:3001/api/transactions/community/payment_received"
+    );
     const transactions = await data.json(); //convert to JSON format
 
     this.setState({ transactions });
@@ -21,16 +25,20 @@ class Transactions extends Component {
   render() {
     return (
       //map through the transactions array and render each transaction's info to the page
-     <div>
-       {this.state.transactions.map(transaction => (
-         <div>
-           {transaction.username} purchased {transaction.deal_name} for {transaction.amount} {transaction.crypto_symbol} from {transaction.venue_name}
-         </div>
-       ))}
-     </div>
+      <div className="transactions-content">
+        <CryptoRankings />
+        <div>
+          {this.state.transactions.map(transaction => (
+            <div>
+              {transaction.username} purchased {transaction.deal_name} for{" "}
+              {transaction.amount} {transaction.crypto_symbol} from{" "}
+              {transaction.venue_name}
+            </div>
+          ))}
+        </div>
+      </div>
     );
   }
 }
 
 export default Transactions;
-
