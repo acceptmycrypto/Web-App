ec6dc8278efefa8dd21dc74ac92d4f92609d9c58
Simon Nguyen <simonnguyen3054@gmail.com>
2018-10-23 12:49:13 -0700

    FeedCard component

--- client/src/components/Feed/Transactions/Transactions.js
+++ client/src/components/Feed/Transactions/Transactions.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Transactions.css";
 import CryptoRankings from "../../CryptosRanking";
+import FeedCard from "../../UI/FeedCard/FeedCard";
 
 class Transactions extends Component {
   constructor() {
@@ -24,31 +25,9 @@ class Transactions extends Component {
 
   render() {
     return (
-      //map through the transactions array and render each transaction's info to the page
       <div className="transactions-content">
         <CryptoRankings />
-
-        <div className="right">
-          {this.state.transactions.map((transaction, i) => (
-            <div key={i} className="transaction-card">
-              <div className="card-content">
-                <div className="user-info">
-                  <div className="user-icon-background">
-                    <i class="user-icon fas fa-user-circle" />
-                  </div>
-                  <div className="user-name">{transaction.username}</div>
-                </div>
-                <div className="user-transaction-info">
-                  <div>
-                    Purchased {transaction.deal_name} for {transaction.amount}{" "}
-                    {transaction.crypto_symbol} from {transaction.venue_name}
-                  </div>
-                  <div className="timestamp"><small>{transaction.date_purchased}</small></div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <FeedCard transactions={this.state.transactions}/>
       </div>
     );
   }
