d48f32e82fcaa7a67e955cf4b8bd3daef6eb4674
Simon Nguyen <simonnguyen3054@gmail.com>
2018-10-23 12:12:56 -0700

    complete styling transaction card

--- client/src/components/Feed/Transactions/Transactions.js
+++ client/src/components/Feed/Transactions/Transactions.js
@@ -27,12 +27,25 @@ class Transactions extends Component {
       //map through the transactions array and render each transaction's info to the page
       <div className="transactions-content">
         <CryptoRankings />
-        <div>
-          {this.state.transactions.map(transaction => (
-            <div>
-              {transaction.username} purchased {transaction.deal_name} for{" "}
-              {transaction.amount} {transaction.crypto_symbol} from{" "}
-              {transaction.venue_name}
+
+        <div className="right">
+          {this.state.transactions.map((transaction, i) => (
+            <div key={i} className="transaction-card">
+              <div className="card-content">
+                <div className="user-info">
+                  <div className="user-icon-background">
+                    <i class="user-icon fas fa-user-circle" />
+                  </div>
+                  <div className="user-name">{transaction.username}</div>
+                </div>
+                <div className="user-transaction-info">
+                  <div>
+                    Purchased {transaction.deal_name} for {transaction.amount}{" "}
+                    {transaction.crypto_symbol} from {transaction.venue_name}
+                  </div>
+                  <div className="timestamp"><small>{transaction.date_purchased}</small></div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
