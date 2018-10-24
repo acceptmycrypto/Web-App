be20f113a66287ca26180d1ecdfebe804945d636
avakrishn <avakrishn@gmail.com>
2018-10-24 12:23:43 -0700

    update profile card component with key attribute for divs rendered using .map function

--- client/src/components/Profile/ProfileCard/ProfileCard.js
+++ client/src/components/Profile/ProfileCard/ProfileCard.js
@@ -4,19 +4,19 @@ import "./ProfileCard.css";
 const ProfileCard = props => {
     return (
         <div>
-            {props.user_info.map((x) =>
-                <div id="profile" data-id={x.id} className="p-3 pb-0 ml-3 d-flex flex-column">
-                    {(props.user_info.photo === undefined)
-                        ? <i className="fas fa-user-circle my-2 py-4 px-1 shaded"></i>
-                        : <img src={props.user_info.photo}></img>
+            {props.user_info.map((userData, i) =>
+                <div id="profile" data-id={userData.id} className="p-3 pb-0 ml-3 d-flex flex-column" key={"user" + i}>
+                    {(userData.photo.indexOf("fa-user") !== -1)
+                        ? <i className={'fas my-2 py-4 px-1 shaded ' + userData.photo}></i>
+                        : <img src={userData.photo}></img>
                     }
 
-                    {/* <img id="responsive" data-id={x.id} clasName="my-2 justify-content-center" src={this.state.src} alt="" /> */}
-                    <h5 className="my-2 blueText">{x.username}</h5>
-                    <h5 className="my-2 capitalize blueText">{x.first_name}  {x.last_name}</h5>
-                    <h5 className="my-2 blueText"> <i className="fas fa-map-marker-alt mr-1"></i>  {x.user_location}</h5>
-                    <h5 className="my-2 blueText"> <i className="fas fa-envelope mr-1"></i> <a className="blueText" href={`mailto:${x.email}`}>{x.email}</a></h5>
-                    <h5 className="my-2 font-italic blueText">{x.bio}</h5>
+                    {/* <img id="responsive" data-id={userData.id} clasName="my-2 justify-content-center" src={this.state.src} alt="" /> */}
+                    <h5 className="my-2 blueText font-15">{userData.username}</h5>
+                    <h5 className="my-2 capitalize blueText font-15">{userData.first_name}  {userData.last_name}</h5>
+                    <h5 className="my-2 blueText font-15"> <i className="fas fa-map-marker-alt mr-1"></i>  {userData.user_location}</h5>
+                    <h5 className="my-2 blueText font-15"> <i className="fas fa-envelope mr-1"></i> <a className="blueText link" href={`mailto:${userData.email}`}>{userData.email}</a></h5>
+                    <h5 className="my-2 font-italic blueText font-15">{userData.bio}</h5>
                 </div>
             )}
         </div>
