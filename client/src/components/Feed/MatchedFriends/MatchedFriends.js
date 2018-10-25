b9cfe8f7b917c0b7fc39c35d38d5cbc8c4233ff6
avakrishn <avakrishn@gmail.com>
2018-10-24 17:49:14 -0700

    create matched friends feed

--- client/src/components/Feed/MatchedFriends/MatchedFriends.js
+++ client/src/components/Feed/MatchedFriends/MatchedFriends.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import "./MatchedFriends.css";
+import CryptoRankings from "../../CryptosRanking";
+import FeedCard from "../FeedCard";
 
 class MatchedFriends extends Component {
-    constructor() {
-      super();
-  
-      this.state = {
-      }
-    }
-  
-  
-    render() {
-      console.log(this.state);
-      return (
-        <div className="MatchedFriends">
-        
-        </div>
-      );
-    }
+  constructor() {
+    super();
+
+    this.state = {
+      friends_transactions: []
+    };
   }
-  
-  export default MatchedFriends;
+
+  async componentDidMount() {
+
+    const data = await fetch("http://localhost:3001/matchedFriends/feed");
+    const friends_transactions = await data.json(); //convert to JSON format
+
+    this.setState({ friends_transactions });
+  }
+
+  render() {
+    return (
+      <div className="transactions-content">
+        <CryptoRankings />
+        <div className="w-50">
+          <FeedCard transactions={this.state.friends_transactions}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MatchedFriends;
