3d4a9a4aac15498e68e71b09bcdcaa9f3346f523
Hooman <36459689+hooman64@users.noreply.github.com>
2018-10-26 23:52:50 -0700

    Dropdown Notification + Updated CSS

--- client/src/components/Navigation/Notification/Notification.js
+++ client/src/components/Navigation/Notification/Notification.js
@@ -1,21 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Notification.css";
+import React from 'react';
+import './Notification.css';
 
-const Notification = props => {
-  return (
-      <div className="box">
-        <div className="notifications">
-            <i className="fas fa-bell"></i>
-            <span className="num">4</span>
-            <ul>
-                <li>
-                    <span className='text'>Place Holder</span>
-                </li>
-            </ul>
-        </div>
-      </div>
-  );
+
+class Notification extends React.Component {
+constructor(){
+ super();
+
+ this.state = {
+       displayMenu: false,
+     };
+
+  this.showDropdownMenu = this.showDropdownMenu.bind(this);
+  this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+
 };
+
+showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+    document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+
+  }
+
+  render() {
+    return (
+        <div  className="dropdown_notif">
+	        
+            <i className="fas fa-bell button" onClick={this.showDropdownMenu}></i>
+            <span className="num_notif">4</span>
+          
+          { this.state.displayMenu ? (
+          <ul className="ul_notif">
+            <li><a className="active_notif" href="/notifications">Notification Placeholder</a></li>
+            <li><a className="all_notif" href="/">All Notifications</a></li>
+          </ul>
+        ):
+        (
+          null
+        )
+        }
+
+	      </div>
+
+    );
+  }
+
+}
 
 export default Notification;
