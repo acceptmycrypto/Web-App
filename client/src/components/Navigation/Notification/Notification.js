081d2a6f4db3927a2e7b2570ebd86f0a32e2ad21
Hooman <36459689+hooman64@users.noreply.github.com>
2018-10-28 17:26:58 -0700

    Fixed UL box size and Hovering

--- client/src/components/Navigation/Notification/Notification.js
+++ client/src/components/Navigation/Notification/Notification.js
@@ -38,15 +38,14 @@ showDropdownMenu(event) {
           
           { this.state.displayMenu ? (
           <ul className="ul_notif">
-            <li><a className="active_notif" href="/">Notification Placeholder</a></li>
-            <li><a className="all_notif" href="/notifications">All Notifications</a></li>
+            <li className="li_notif"><a className="active_notif" href="/">Notification Placeholder</a></li>
+            <li className="li_notif"><a className="all_notif" href="/notifications">All Notifications</a></li>
           </ul>
         ):
         (
           null
         )
         }
-
 	      </div>
 
     );
