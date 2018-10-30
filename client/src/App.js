22e9f4ca32e61926ae3706992ea9ea21e8be6a5c
avakrishn <avakrishn@gmail.com>
2018-10-30 12:55:18 -0700

    testing redirect component. need to modify with jwt

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Switch, Route, Link, Redirect } from "react-router-dom";
 
 import SignUp from "./components/signup/SignUp";
 import SignIn from "./components/signup/SignIn";
 
 import Profile from "./components/Profile/UserProfile";
-import Friend from "./components/Profile/FriendProfile"
+import Friend from "./components/Profile/FriendProfile";
+import Settings from "./components/UserSettings/Settings"
 import Crypto from "./components/Crypto";
 import FeedDeals from "./components/Feed/Deals";
 import DealItem from "./components/Feed/Deals/DealItem";
@@ -23,7 +24,12 @@ class App extends Component {
             <Route exact path="/" component={SignIn} />
             <Route path="/SignUp" component={SignUp} />
             <Route exact path="/profile" component={Profile} />
-            <Route path="/profile/:id" component={Friend} />
+
+            <Route path="/profile/:id" component={ (props) => < Friend {...props}/>} />
+
+            {/* Redirects to profile page when id matches user id, hard coded user id 1 for now */}
+            <Redirect from='profile/1' to='/profile' />
+            <Route path ="/settings" component={Settings} />
             <Route path="/crypto" component={Crypto} />
 
             <Route exact path="/feed/deals" component={FeedDeals} />
