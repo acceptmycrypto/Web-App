e0a0bc891921b5b7d005a6de7706c7a7aed9c206
avakrishn <avakrishn@gmail.com>
2018-10-31 16:30:20 -0700

    check if userData properties exist

--- client/src/components/Profile/ProfileCard/ProfileCard.js
+++ client/src/components/Profile/ProfileCard/ProfileCard.js
@@ -13,10 +13,21 @@ const ProfileCard = props => {
 
                     {/* <img id="responsive" data-id={userData.id} clasName="my-2 justify-content-center" src={this.state.src} alt="" /> */}
                     <h5 className="my-2 blueText font-15">{userData.username}</h5>
-                    <h5 className="my-2 capitalize blueText font-15">{userData.first_name}  {userData.last_name}</h5>
-                    <h5 className="my-2 blueText font-15"> <i className="fas fa-map-marker-alt mr-1"></i>  {userData.user_location}</h5>
+                    {(userData.first_name !== null &&  userData.last_name !== null)
+                        ? <h5 className="my-2 capitalize blueText font-15">{userData.first_name}  {userData.last_name}</h5>
+                        : null
+                    }
+                    {(userData.user_location !== null)
+                        ? <h5 className="my-2 blueText font-15"> <i className="fas fa-map-marker-alt mr-1"></i>  {userData.user_location}</h5>
+                        : null
+                    }
+
                     <h5 className="my-2 blueText font-15"> <i className="fas fa-envelope mr-1"></i> <a className="blueText link" href={`mailto:${userData.email}`}>{userData.email}</a></h5>
-                    <h5 className="my-2 font-italic blueText font-15">{userData.bio}</h5>
+                    {(userData.bio !== null)
+                        ? <h5 className="my-2 font-italic blueText font-15">{userData.bio}</h5>
+                        : null
+                    }
+                    
                 </div>
             )}
         </div>
