ac4a3560327a1df1cbc6b89ea04c5ee6a2d83346
avakrishn <avakrishn@gmail.com>
2018-11-02 12:46:03 -0700

    add Layout back into CryptoForum

--- client/src/components/Community/CryptoForum/CryptoForum.js
+++ client/src/components/Community/CryptoForum/CryptoForum.js
@@ -112,6 +112,7 @@ class CryptoForum extends Component {
     render() {
         return (
             <div>
+                <Layout/>
                 <h1>{this.state.cryptoName}</h1>
                 {this.state.allComments.map(parent => 
                     <div className="parentComment" id={"parent"+parent.id} key={"parent"+parent.id} data-id={parent.id} data-parent={true}>
