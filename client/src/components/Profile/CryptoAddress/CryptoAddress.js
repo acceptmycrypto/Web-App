ac0996974f7a259d80b7486fb55c150226bb00cb
avakrishn <avakrishn@gmail.com>
2018-11-02 16:48:35 -0700

    set field to required

--- client/src/components/Profile/CryptoAddress/CryptoAddress.js
+++ client/src/components/Profile/CryptoAddress/CryptoAddress.js
@@ -7,7 +7,7 @@ const CryptoAddress = props => {
 
         <div className="addressForm d-flex flex-column">
             <form id="addAddressForm" onSubmit={props.updateCryptos}>
-                <input className="mx-3" id="addressFormInput" type="text" name="crypto_address" placeholder="Enter address" />
+                <input className="mx-3" id="addressFormInput" type="text" name="crypto_address" placeholder="Enter address" required />
 
                 <button className="addAddressButton btn btn-outline-primary btn-sm my-2">Add Address</button>
             </form>
