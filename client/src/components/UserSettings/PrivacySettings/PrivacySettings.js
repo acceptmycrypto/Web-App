0e10222ca7aa86a98c8a22a80cdeef33e1b7d588
avakrishn <avakrishn@gmail.com>
2018-11-03 09:52:11 -0700

    create Privacy Settings component

---
+++ client/src/components/UserSettings/PrivacySettings/PrivacySettings.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import "./PrivacySettings.css";
+
+class PrivacySettings extends Component {
+
+    render() {
+
+
+        return (
+            <div className="w-100 mx-0 text-center">
+
+                    <h1 className="text-center lightBlueText">Privacy Settings</h1>
+            </div>
+        );
+    }
+}
+
+
+export default PrivacySettings;
